f30d5f8b025e9cbc20e1c19f73c44a9654c0aa35
לוי מאיר קלנסי <32032952+levimeirclancy@users.noreply.github.com>
2025-01-21 15:36:37 -0800

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,10 @@
+<?
 
+if !($_SESSION['csv_contents'])):
+ $csv_contents = file_get_contents('https://docs.google.com/spreadsheets/d/e/2PACX-1vT2qh0dMlZ6gI8aEqqMjQt6ZS6U-sYOX3AKOHz1ajVgiJ0PfiNAQpp5B1-a8yAZ-e81DPMCs-b-aS8H/pub?output=csv');
+ $_SESSION['csv_contents'] = $csv_contents;
+endif;
+
+print_r($csv_contents);
+
+?>
