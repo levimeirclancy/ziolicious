23f6b5681e3ebe4cde8b684816a9605febbd9334
לוי מאיר קלנסי <32032952+levimeirclancy@users.noreply.github.com>
2025-06-19 17:45:07 -0700

    Update style-hashtags.php

--- style-hashtags.php
+++ style-hashtags.php
@@ -4,10 +4,84 @@
 
 $css_array = [
 
-":root"	=> [
-	"--bg"				=> "rgb(215,215,215)",
-	"--text"			=> "#111",
-	"--accent"			=> "#111",
+".hashtag-white" => [
+	"background"			=> "linear-gradient(45deg,rgba(255, 255, 255, 1), rgba(215, 215, 215, 1))",
+	],
+
+".hashtag-blue" => [
+	"background"			=> "linear-gradient(45deg,rgba(66, 203, 245,1), rgba(0, 75, 181, 1))",
+	],
+
+".hashtag-architecture" => [
+	"background"			=> "rgba(130,130,130,1)",
+	],
+
+".hashtag-jerusalem" => [
+	"background"			=> "linear-gradient(45deg,rgba(175, 145, 95,1), rgba(190, 120, 45, 1))",
+	],
+
+".hashtag-judaica" => [
+	"background"			=> "linear-gradient(45deg,rgba(175, 145, 95,1), rgba(190, 120, 45, 1))",
+	],
+
+".hashtag-landscapes" => [
+	"background"			=> "linear-gradient(180deg, rgba(20, 115, 205, 1), rgba(35, 110, 20, 1))",
+	],
+
+".hashtag-families" => [
+	"background"			=> "green",
+	],
+
+".hashtag-beach" => [
+	"background"			=> "#00b4d8",
+	],
+
+".hashtag-sunset" => [
+	"background"			=> "linear-gradient(45deg,#ff6a00,#ee0979)",
+	],
+
+".hashtag-queeridentity" => [
+	"background"			=> "linear-gradient(0deg,rgba(225,65,245,1),rgba(95,15,185,1))",
+	],
+
+".hashtag-pomegranate" => [
+	"background"			=> "#c0392b",
+	],
+
+".hashtag-nsfw" => [
+	"background"			=> "red",
+	],
+
+".hashtag-nonbinary" => [
+	"background"			=> "red",
+	],
+
+".hashtag-lesbians" => [
+	"background"			=> "red",
+	],
+
+".hashtag-blackjewish" => [
+	"background"			=> "linear-gradient(90deg,#ff69b4,#ffa500)",
+	],
+
+".hashtag-trans" => [
+	"background"			=> "linear-gradient(90deg,rgba(40,130,165,1),rgba(161, 84, 99,1))",
+	],
+
+".hashtag-graffiti" => [
+	"background"			=> "linear-gradient(90deg,#ff69b4,#ffa500)",
+	],
+
+".hashtag-jerusalemstone" => [
+	"background"			=> "#d9c4a1;",
+	],
+
+".hashtag-telaviv" => [
+	"background"			=> "linear-gradient(-10deg,rgba(250, 185, 72,1), rgba(00, 141, 196, 1))",
+	],
+
+".hashtag-sky" => [
+	"background"			=> "#0077b6",
 	],
 
 ];
