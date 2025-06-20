6a213b710d66d7f3afa9977fab0ee425474211a8
לוי מאיר קלנסי <32032952+levimeirclancy@users.noreply.github.com>
2025-06-19 20:18:07 -0700

    Update index.php

--- index.php
+++ index.php
@@ -16,9 +16,24 @@ foreach ($url_array_raw as $key_temp => $url_temp):
 	if ($key_temp > 4): break; endif;
 	if (empty($url_temp)): continue; endif;
 	$url_array[] = $url_temp;
-	endforach;
+	endforeach;
 
-print_r($url_array); exit;
+if (empty($url_array[0])):
+	include_once('index-home.php');
+	exit;
+elseif (in_array($url_array[0], ["style-global.css", "style-hashtags.css", "style-photo.css", "style-transitions.css"])):
+	include_once(str_replace(".css", ".php", $url_chooser));
+	echo css_process($css_array);
+	exit;
+elseif ($url_array[0] == "about"):
+	include_once('index-about.php');
+	exit;
+elseif ($url_array[0] == "shop"):
+	include_once('index-shop.php');
+	exit;
+	endif;
 
+include_once('index-404.php');
+exit;
 
 ?>
