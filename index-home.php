8ea6cd922250958e1c2481ca816ba28b4423f89f
לוי מאיר קלנסי <32032952+levimeirclancy@users.noreply.github.com>
2025-06-19 19:24:38 -0700

    Update index-home.php

--- index-home.php
+++ index-home.php
@@ -4,4 +4,224 @@ if ($index_temp !== "valid"): exit; endif;
 
 	// 404 design
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Ziolicious</title>
+
+  <!-- Fonts -->
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Monoton&family=Tilt+Warp&family=Righteous&family=Noto+Sans+Mono:wght@100..900&family=Playpen+Sans+Hebrew:wght@100..800&family=Mrs+Sheppards&family=Rubik+Maps&family=Rubik+Puddles&family=Secular+One&family=Solitreo&display=swap" rel="stylesheet">
+
+<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0&icon_names=download" />
+
+
+<link rel="stylesheet" type="text/css" href="style_hashtags.css">
+
+<link rel="stylesheet" type="text/css" href="style_global.css">
+
+<link rel="stylesheet" type="text/css" href="style_transitions.css">
+
+</head>
+
+<body>
+
+<header>
+  <span class="header-logo"><a href="ziolicious_index.html">Ziolicious</a></span>
+  <input class="search-box" type="text" placeholder="&#x1F50D;  Search">
+</header>
+
+
+<!--<div class='body-tagline'>Wholly, unapologetically Jewish.</div>-->
+<div class='body-tagline'>Stories in color. Stories in context.</div>
+
+<div class='body-tagline-description'>Immersive micro-stories showing whole, unapologetic Jewish life. All content is free for editorial use by Zionist creators. Still confused? <a href='/about/'><span class='link-animation'>Learn more</span></a></div>
+
+
+<main>
+
+<div class='body-sections'>
+
+<!-- View all tags -->
+
+
+  <!-- Section Example -->
+  <div class='body-section-background' style='background-image: url("https://images.unsplash.com/photo-1621874098652-168c080d322f?q=80&w=1287&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D");'>
+  <div class='body-section body-section-overlay'>
+    <div class='section-header'>
+<a class="hashtag-link hashtag-link-transition-two hashtag-telaviv" href="#"><span class='hashtag-symbol'>#</span><span class='hashtag-text'>TelAviv</span></a></div>
+    <p class="section-description">Color, energy, and calm at the Mediterranean’s edge.</p>
+
+    <div class="photo-row">
+
+      <a href="ziolicious_photo.html"><div class="photo-row-img-container photo-row-img-transition-one" style="background-image: url('https://images.unsplash.com/photo-1621874098652-168c080d322f?q=80&w=1287&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D');"></div></a>
+
+      <div class="photo-row-img-container photo-row-img-transition-one" style="background-image: url('https://cdn.myportfolio.com/edcae732-6d45-4207-bf20-5f1fad45d9f8/97074001-bd9d-46ea-ab00-d0fa3cff5d3d_rw_1200.jpg?h=e11f505c82d0f061611cb397b9ca090f');"></div>
+
+      <div class="photo-row-img-container photo-row-img-transition-two" style="background-image: url('https://cdn.myportfolio.com/edcae732-6d45-4207-bf20-5f1fad45d9f8/3078bc6c-104a-40ac-bfe9-3b3c2ae303a2_rw_1200.jpg?h=9a0e223fff3a1bb2034c5e52e6423d01');"></div>
+
+      <div class="photo-row-img-container photo-row-img-transition-one" style="background-image: url('https://cdn.myportfolio.com/edcae732-6d45-4207-bf20-5f1fad45d9f8/60fa597f-a39c-4e23-90a6-fb74a23c61c3_rw_1920.jpg?h=4bf60bd6049725359060e277127fc254');"></div>
+
+
+
+    </div>
+  </div>
+</div>
+
+
+
+
+
+
+  <!-- Tag Cloud -->
+  <div class='body-section-background hashtag-queeridentity'>
+
+  <div class='body-section'>
+    <div class='section-header'>
+
+<a class="hashtag-link hashtag-link-transition-one hashtag-white" href="#"><span class="hashtag-symbol">#</span><span class="hashtag-text">QueerIdentity</span></a></div>
+    <div class="tag-cloud">
+      <a class="hashtag-link hashtag-link-transition-two hashtag-white" href="#"><span class="hashtag-symbol">#</span><span class="hashtag-text">NSFW</span></a>
+      <a class="hashtag-link hashtag-link-transition-two hashtag-white" href="#"><span class="hashtag-symbol">#</span><span class="hashtag-text">couples</span></a>
+      <a class="hashtag-link hashtag-link-transition-one hashtag-white" href="#"><span class="hashtag-symbol">#</span><span class="hashtag-text">families</span></a>
+      <a class="hashtag-link hashtag-link-transition-two hashtag-white" href="#"><span class="hashtag-symbol">#</span><span class="hashtag-text">trans</span></a>
+      <a class="hashtag-link hashtag-link-transition-one hashtag-white" href="#"><span class="hashtag-symbol">#</span><span class="hashtag-text">nonbinary</span></a>
+
+      <a class="hashtag-link hashtag-link-transition-three hashtag-white" href="#"><span class="hashtag-symbol">#</span><span class="hashtag-text">advocacy</span></a>
+      <a class="hashtag-link hashtag-link-transition-one hashtag-white" href="#"><span class="hashtag-symbol">#</span><span class="hashtag-text">lesbians</span></a>
+    </div>
+  </div>
+
+</div>
+
+
+
+
+
+
+  <div class='body-section-background' style='background-image: url("https://cdn.myportfolio.com/edcae732-6d45-4207-bf20-5f1fad45d9f8/8c15e766-861d-4ffc-85ad-d841942e5a30_rw_1200.jpg?h=37e4c970d08a15ac361a149dc59f3466");'>
+
+  <div class='body-section body-section-overlay'>
+    <div class='section-header'><a class="hashtag-link hashtag-link-transition-three hashtag-blackjewish" href="#"><span class='hashtag-symbol'>#</span><span class='hashtag-text'>BlackJewish</span></a>
+<a class="hashtag-link hashtag-link-transition-two hashtag-families" href="#"><span class="hashtag-symbol">#</span><span class="hashtag-text">families</span></a></div>
+    <p class="section-description">Joy and beauty across generations, rooted in heritage and home.</p>
+    <div class="photo-row">
+
+
+      <div class="photo-row-img-container photo-row-img-transition-three" style="background-image: url('https://cdn.myportfolio.com/edcae732-6d45-4207-bf20-5f1fad45d9f8/8c15e766-861d-4ffc-85ad-d841942e5a30_rw_1200.jpg?h=37e4c970d08a15ac361a149dc59f3466');"></div>
+
+      <div class="photo-row-img-container photo-row-img-transition-one" style="background-image: url('https://cdn.myportfolio.com/edcae732-6d45-4207-bf20-5f1fad45d9f8/9bbe2424-dbf5-4232-9aef-d0fa1030a54f_rw_600.jpg?h=f0881407ab26553b4913a01c179a75e3');"></div>
+
+      <div class="photo-row-img-container photo-row-img-transition-two" style="background-image: url('https://cdn.myportfolio.com/edcae732-6d45-4207-bf20-5f1fad45d9f8/88d49c26-0e18-4dde-a8d7-639f651f41e7_rw_1200.jpg?h=1fa631133d6f9c7cfbff286b9ade9a3c');"></div>
+
+      <div class="photo-row-img-container photo-row-img-transition-three" style="background-image: url('https://cdn.myportfolio.com/edcae732-6d45-4207-bf20-5f1fad45d9f8/8c15e766-861d-4ffc-85ad-d841942e5a30_rw_1200.jpg?h=37e4c970d08a15ac361a149dc59f3466');"></div>
+
+    </div>
+</div>
+
+
+  </div>
+
+
+
+
+
+  <!-- Tag Cloud -->
+  <div class='body-section-background hashtag-landscapes'>
+
+  <div class='body-section'>
+    <div class='section-header'><a class="hashtag-link hashtag-link-transition-one hashtag-white" href="#"><span class="hashtag-symbol">#</span><span class="hashtag-text">landscapes</span></a></div>
+    <div class="tag-cloud">
+      <a class="hashtag-link hashtag-link-transition-two hashtag-white" href="#"><span class="hashtag-symbol">#</span><span class="hashtag-text">beach</span></a>
+      <a class="hashtag-link hashtag-link-transition-two hashtag-white" href="#"><span class="hashtag-symbol">#</span><span class="hashtag-text">sunset</span></a>
+      <a class="hashtag-link hashtag-link-transition-one hashtag-white" href="#"><span class="hashtag-symbol">#</span><span class="hashtag-text">graffiti</span></a>
+      <a class="hashtag-link hashtag-link-transition-three hashtag-white" href="#"><span class="hashtag-symbol">#</span><span class="hashtag-text">sky</span></a>
+      <a class="hashtag-link hashtag-link-transition-one hashtag-white" href="#"><span class="hashtag-symbol">#</span><span class="hashtag-text">pomegranate</span></a>
+    </div>
+  </div>
+<div>
+
+</div>
+
+</div>
+
+
+
+
+
+
+
+  <div class='body-section-background' style='background-image: url("https://cdn.myportfolio.com/edcae732-6d45-4207-bf20-5f1fad45d9f8/a61ed5cd-0b24-45dd-82e9-a1341314b93b_rw_1920.jpg?h=f7e593cc8f314d4f629ff87b14be957f");'>
+
+  <div class='body-section body-section-overlay'>
+    <div class='section-header'><a class="hashtag-link hashtag-link-transition-three hashtag-architecture" href="#"><span class='hashtag-symbol'>#</span><span class='hashtag-text'>architecture</span></a>
+<a class="hashtag-link hashtag-link-transition-two hashtag-jerusalem" href="#"><span class="hashtag-symbol">#</span><span class="hashtag-text">Jerusalem</span></a></div>
+    <p class="section-description">Joy and beauty across generations, rooted in heritage and home.</p>
+    <div class="photo-row">
+
+
+      <div class="photo-row-img-container photo-row-img-transition-two" style="background-image: url('https://cdn.myportfolio.com/edcae732-6d45-4207-bf20-5f1fad45d9f8/a61ed5cd-0b24-45dd-82e9-a1341314b93b_rw_1920.jpg?h=f7e593cc8f314d4f629ff87b14be957f');"></div>
+
+
+      <div class="photo-row-img-container photo-row-img-transition-one" style="background-image: url('https://cdn.myportfolio.com/edcae732-6d45-4207-bf20-5f1fad45d9f8/6c55d336-ba36-49a6-8cc8-f1cb8c475446_rw_1920.jpg?h=985bb88d7bd0e27ef6b5cd7ae6882d4e');"></div>
+
+
+      <div class="photo-row-img-container photo-row-img-transition-two" style="background-image: url('https://cdn.myportfolio.com/edcae732-6d45-4207-bf20-5f1fad45d9f8/7c8465bf-0b77-4c65-aea5-afc59cde0685_rw_1920.jpg?h=7384c76b09670546ea150fd83d89f2a9');"></div>
+
+      <div class="photo-row-img-container photo-row-img-transition-two" style="background-image: url('https://cdn.myportfolio.com/edcae732-6d45-4207-bf20-5f1fad45d9f8/a61ed5cd-0b24-45dd-82e9-a1341314b93b_rw_1920.jpg?h=f7e593cc8f314d4f629ff87b14be957f');"></div>
+
+
+
+
+    </div>
+</div>
+
+
+  </div>
+
+
+
+
+
+
+  <!-- Tag Cloud -->
+  <div class='body-section-background hashtag-judaica'>
+
+  <div class='body-section'>
+    <div class='section-header'><a class="hashtag-link hashtag-link-transition-one hashtag-white" href="#"><span class="hashtag-symbol">#</span><span class="hashtag-text">judaica</span></a></div>
+    <div class="tag-cloud">
+      <a class="hashtag-link hashtag-link-transition-two hashtag-white" href="#"><span class="hashtag-symbol">#</span><span class="hashtag-text">Passover</span></a>
+      <a class="hashtag-link hashtag-link-transition-two hashtag-white" href="#"><span class="hashtag-symbol">#</span><span class="hashtag-text">Hanukah</span></a>
+      <a class="hashtag-link hashtag-link-transition-one hashtag-white" href="#"><span class="hashtag-symbol">#</span><span class="hashtag-text">Shavuot</span></a>
+      <a class="hashtag-link hashtag-link-transition-three hashtag-white" href="#"><span class="hashtag-symbol">#</span><span class="hashtag-text">Rosh HaShanah</span></a>
+      <a class="hashtag-link hashtag-link-transition-one hashtag-white" href="#"><span class="hashtag-symbol">#</span><span class="hashtag-text">Torah</span></a>
+    </div>
+  </div>
+<div>
+
+</div>
+
+</div>
+
+
+</div>
+
+</main>
+
+
+<footer>
+  <a href="#">Instagram</a> |
+  <a href="#">X (Twitter)</a> |
+  <a href="#">Email</a> |
+  <a href="#">Advertise</a>
+  <a href="#">hello@ziolicious.com</a>
+</footer>
+
+</body>
+</html>
+
+
 ?>
