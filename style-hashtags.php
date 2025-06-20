c88865721c2015df79bd082075b1d969b3afe2da
לוי מאיר קלנסי <32032952+levimeirclancy@users.noreply.github.com>
2025-06-19 17:40:30 -0700

    Create style-hashtags.php

---
+++ style-hashtags.php
@@ -0,0 +1,20 @@
+<?
+
+<?
+
+$css_array = [
+
+":root"	=> [
+	"--bg"				=> "rgb(215,215,215)",
+	"--text"			=> "#111",
+	"--accent"			=> "#111",
+	],
+
+];
+ 
+$html_output .= css_process($css_array);
+
+?>
+
+
+?>
