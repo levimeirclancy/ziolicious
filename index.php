40d67e118e634f77106143e63b71bf5c2ac194f6
לוי מאיר קלנסי <32032952+levimeirclancy@users.noreply.github.com>
2025-06-19 19:38:14 -0700

    Update index.php

--- index.php
+++ index.php
@@ -11,6 +11,10 @@ $index_temp = "valid";
 
 if (empty($url_chooser)):
 	include_once('index-home.php');
+if (in_array($url_chooser, ["style-global.css", "style-hashtags.css", "style-photo.css", "style-transitions.css"])):
+	include_once(str_replace(".css", ".php", $url_chooser));
+	echo css_process($css_array);
+	exit;
 elseif (in_array($url_chooser, $post_id_array)):
 	include_once('index-entry.php');
 elseif ($url_chooser == "about"):
