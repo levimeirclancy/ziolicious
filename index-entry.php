e4ca4f65cd63ef7484e0165d3de4da33aefaced2
לוי מאיר קלנסי <32032952+levimeirclancy@users.noreply.github.com>
2025-06-19 19:40:26 -0700

    Update and rename index-entry.php to index-photo.php

--- index-entry.php
+++
@@ -1,7 +0,0 @@
-<?
-
-if ($index_temp !== "valid"): exit; endif;
-
-		// 404 design
-
-?>
---
+++ index-photo.php
@@ -0,0 +1,93 @@
+<?
+
+if ($index_temp !== "valid"): exit; endif;
+
+// 404 design
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Ziolicious</title>
+
+  <!-- Fonts -->
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Monoton&family=Tilt+Warp&family=Righteous&family=Noto+Sans+Mono:wght@100..900&family=Playpen+Sans+Hebrew:wght@100..800&family=Mrs+Sheppards&family=Rubik+Maps&family=Rubik+Maze&family=Rubik+Puddles&family=Rubik+Scribble&family=Secular+One&family=Solitreo&display=swap" rel="stylesheet">
+
+<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0&icon_names=download" />
+
+<link rel="stylesheet" type="text/css" href="style_hashtags.css">
+
+<link rel="stylesheet" type="text/css" href="style_global.css">
+
+<link rel="stylesheet" type="text/css" href="style_transitions.css">
+
+<link rel="stylesheet" type="text/css" href="style_photo.css">
+
+</head>
+
+<body>
+
+<header>
+  <span class="header-logo"><a href="ziolicious_index.html">Ziolicious</a></span>
+  <input class="search-box" type="text" placeholder="&#x1F50D;  Search">
+</header>
+
+<main>
+
+<div class="photo-splash-img-background" style="background-image: url('https://images.unsplash.com/photo-1621874098652-168c080d322f?q=80&w=1287&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D');">
+
+<div class="photo-splash-img-overlay" style='background:conic-gradient(0,rgba(7,28,76,0.4) 20%,rgba(37,88,147,0.1) 50%, rgba(37,88,147,0.4) 100%);'>
+
+<div class="photo-splash-img-fadeout"></div>
+
+<img class='photo-splash-img' src='https://images.unsplash.com/photo-1621874098652-168c080d322f?q=80&w=1287&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' alt='' />
+
+
+</div>
+
+
+</div>
+
+<div class='photo-splash-button-container'>
+
+<div class='photo-splash-button'><span class="material-symbols-outlined">download</span><span>High res</span></div>
+
+<div class='photo-splash-button'><span class="material-symbols-outlined">download</span><span>Compressed</span></div>
+
+<div class='photo-splash-button'><span class="material-symbols-outlined">download</span><span>Raw</span></div>
+
+</div>
+
+<div class='photo-splash-story'>
+
+<div class='photo-splash-story-caption' style='background:linear-gradient(135deg, rgba(7,28,76,1), rgba(37,88,147,1)); -webkit-background-clip:text;color:transparent;'>
+
+I was walking along the coast of Tel Aviv. And when I looked up, I saw this beautiful flag. The land, the flag, the identity, all felt whole. This has been one of my more successful photos on Unsplash, used by a wide range of people.
+
+</div>
+
+</div>
+
+
+
+
+</main>
+
+
+<footer>
+  <a href="#">Instagram</a> |
+  <a href="#">X (Twitter)</a> |
+  <a href="#">Email</a> |
+  <a href="#">Advertise</a>
+  <a href="#">hello@ziolicious.com</a>
+</footer>
+
+</body>
+</html>
+
+
+?>
