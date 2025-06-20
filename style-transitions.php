12892089c97eaebfa2d8035a4070b84c5d621ee1
לוי מאיר קלנסי <32032952+levimeirclancy@users.noreply.github.com>
2025-06-19 19:17:26 -0700

    Update style-transitions.php

--- style-transitions.php
+++ style-transitions.php
@@ -6,103 +6,92 @@ $css_array = [
 	"letter-spacing"		=> "2px",
 	"transition"			=> "letter-spacing 0.5s ease",
 	],
-
 ".header-logo:hover" => [
 	"letter-spacing"		=> "5px",
 	],
 
+
 ".hashtag-link-transition-one" => [
-	"transform: translateY(0px) rotate(0deg);
-	"text-shadow: 15px 15px 4px rgba(0, 0, 0, 0);
-      transition: transform 0.6s ease, text-shadow 0.5s ease, letter-spacing 0.5s ease, text-underline-offset 0.5s ease;
+	"transform"			=> "translateY(0px) rotate(0deg)",
+	"text-shadow"			=> "15px 15px 4px rgba(0, 0, 0, 0)",
+	"transition"			=> "transform 0.6s ease, text-shadow 0.5s ease, letter-spacing 0.5s ease, text-underline-offset 0.5s ease",
+	],
+".hashtag-link-transition-one:hover" => [
+	"letter-spacing"		=> "2px",
+ 	"text-underline-offset"		=> "0.35em",
+	"transform"			=> "translateY(-10px) rotate(5deg)",
+	"text-shadow"			=> "20px 20px 7px rgba(0, 0, 0, 0.05)",
 	],
 
-".hashtag-link-transition-one:hover" => [
-	letter-spacing: 2px;
-text-underline-offset: 0.35em;
-      transform: translateY(-10px) rotate(5deg);
-      text-shadow: 20px 20px 7px rgba(0, 0, 0, 0.05);
-	],
 
 ".hashtag-link-transition-two" => [
-      transform: translateY(0px) rotate(0deg);
-      text-shadow: 15px 15px 4px rgba(0, 0, 0, 0);
-      transition: transform 0.6s ease, text-shadow 0.5s ease, letter-spacing 0.5s ease, text-underline-offset 0.5s ease;
+	"transform"			=> "translateY(0px) rotate(0deg)",
+	"text-shadow"			=> "15px 15px 4px rgba(0, 0, 0, 0)",
+	"transition"			=> "transform 0.6s ease, text-shadow 0.5s ease, letter-spacing 0.5s ease, text-underline-offset 0.5s ease",
 	],
-
 ".hashtag-link-transition-two:hover" => [
-	letter-spacing: 2px;
-text-underline-offset: 0.35em;
-
-      transform: translateY(-8px) rotate(6deg);
-      text-shadow: 20px 20px 7px rgba(0, 0, 0, 0.05);
+	"letter-spacing"		=> "2px",
+ 	"text-underline-offset"		=> "0.35em",
+	"transform"			=> "translateY(-8px) rotate(6deg)",
+	"text-shadow"			=> "20px 20px 7px rgba(0, 0, 0, 0.05)",
 	],
+
 
 ".hashtag-link-transition-three" => [
-      transform: translateY(0) rotate(0deg);
-      text-shadow: 15px 15px 4px rgba(0, 0, 0, 0);
-      transition: transform 0.6s ease, text-shadow 0.5s ease, letter-spacing 0.5s ease, text-underline-offset 0.5s ease;
+	"transform"			=> "translateY(0) rotate(0deg)",
+	"text-shadow"			=> "15px 15px 4px rgba(0, 0, 0, 0)",
+	"transition"			=> "transform 0.6s ease, text-shadow 0.5s ease, letter-spacing 0.5s ease, text-underline-offset 0.5s ease",
 	],
-
 ".hashtag-link-transition-three:hover" => [
-	letter-spacing: 2px;
-text-underline-offset: 0.35em;
-
-      transform: translateY(-12px) rotate(-5deg);
-      text-shadow: 20px 20px 7px rgba(0, 0, 0, 0.05);
+	"letter-spacing"		=> "2px",
+	"text-underline-offset"		=> "0.35em",
+	"transform"			=> "translateY(-12px) rotate(-5deg)",
+	"text-shadow"			=> "20px 20px 7px rgba(0, 0, 0, 0.05)",
 	],
-    
 
 
 ".photo-row-img-transition-one" => [
-      transform: translateY(0) rotate(0deg);
-      box-shadow: 0 10px 20px rgba(0, 0, 0, 0.15);
-      transition: transform 0.5s ease, box-shadow 0.5s ease, transform 0.5s ease;
+	"transform"			=> "translateY(0) rotate(0deg)",
+	"box-shadow"			=> "0 10px 20px rgba(0, 0, 0, 0.15)",
+	"transition"			=> "transform 0.5s ease, box-shadow 0.5s ease, transform 0.5s ease",
 	],
-
-
 " .photo-row-img-transition-one:hover" => [
-      transform: translateY(-25px) rotate(5deg);
-      box-shadow: 0 20px 30px rgba(0, 0, 0, 0.25);
+	"transform"			=> "translateY(-25px) rotate(5deg)",
+	"box-shadow"			=> "0 20px 30px rgba(0, 0, 0, 0.25)",
 	],
 
 
 ".photo-row-img-transition-two" => [
-      transform: translateY(0) rotate(0deg);
-      box-shadow: 0 10px 20px rgba(0, 0, 0, 0.15);
-      transition: transform 0.5s ease, box-shadow 0.5s ease, transform 0.5s ease;
+	"transform"			=> "translateY(0) rotate(0deg)",
+	"box-shadow"			=> "0 10px 20px rgba(0, 0, 0, 0.15)",
+	"transition"			=> "transform 0.5s ease, box-shadow 0.5s ease, transform 0.5s ease",
 	],
-
-
 ".photo-row-img-transition-two:hover" => [
-      transform: translateY(-25px) rotate(-5deg);
-      box-shadow: 0 20px 30px rgba(0, 0, 0, 0.25);
+	"transform"			=> "translateY(-25px) rotate(-5deg)",
+	"box-shadow"			=> "0 20px 30px rgba(0, 0, 0, 0.25)",
 	],
 
 
 ".photo-row-img-transition-three" => [
-      transform: translateY(0) rotate(0deg);
-      box-shadow: 0 10px 20px rgba(0, 0, 0, 0.15);
-      transition: transform 0.5s ease, box-shadow 0.5s ease, transform 0.5s ease;
+	"transform"			=> "translateY(0) rotate(0deg)",
+	"box-shadow"			=> "0 10px 20px rgba(0, 0, 0, 0.15)",
+	"transition"			=> "transform 0.5s ease, box-shadow 0.5s ease, transform 0.5s ease",
+	],
+ ".photo-row-img-transition-three:hover" => [
+	"transform"			=> "translateY(-25px) rotate(5deg)",
+	"box-shadow"			=> "0 20px 30px rgba(0, 0, 0, 0.25)",
 	],
 
-
- ".photo-row-img-transition-three:hover" => [
-      transform: translateY(-25px) rotate(5deg);
-      box-shadow: 0 20px 30px rgba(0, 0, 0, 0.25);
-    } 
 
 ".link-animation" => [
-	text-underline-offset: 0.2em;
-      transform: translateY(0) rotate(0deg);
-	transition: text-underline-offset 0.3s ease, transform 0.6s ease, letter-spacing 0.6s ease;
+	"text-underline-offset"		=> "0.2em",
+	"transform"			=> "translateY(0) rotate(0deg)",
+	"transition"			=> "text-underline-offset 0.3s ease, transform 0.6s ease, letter-spacing 0.6s ease",
 	],
-
 ".link-animation:hover" => [
-letter-spacing: 0px;
- transform: translateY(-5px) rotate(0deg);
-
-text-underline-offset: 0.5em;
+	"letter-spacing"		=> "0px",
+	"transform"			=> "translateY(-5px) rotate(0deg)",
+	"text-underline-offset"		=> "0.5em",
 	],
 	
 ];
