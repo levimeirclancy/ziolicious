ef0eba69ce7f0e421e1b1b2e2709440d256c97d0
לוי מאיר קלנסי <32032952+levimeirclancy@users.noreply.github.com>
2025-06-20 02:40:24 -0700

    Update index_home.php

--- index_home.php
+++ index_home.php
@@ -19,9 +19,6 @@ $html_temp .= '<div class="body-sections">';
 // Album
 $img_array = [
 	'https://images.unsplash.com/photo-1621874098652-168c080d322f?q=80&w=1287&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-	'https://cdn.myportfolio.com/edcae732-6d45-4207-bf20-5f1fad45d9f8/97074001-bd9d-46ea-ab00-d0fa3cff5d3d_rw_1200.jpg?h=e11f505c82d0f061611cb397b9ca090f',
-	'https://cdn.myportfolio.com/edcae732-6d45-4207-bf20-5f1fad45d9f8/3078bc6c-104a-40ac-bfe9-3b3c2ae303a2_rw_1200.jpg?h=9a0e223fff3a1bb2034c5e52e6423d01',
-	'https://cdn.myportfolio.com/edcae732-6d45-4207-bf20-5f1fad45d9f8/60fa597f-a39c-4e23-90a6-fb74a23c61c3_rw_1920.jpg?h=4bf60bd6049725359060e277127fc254',
 	'https://plus.unsplash.com/premium_photo-1674933213970-e270ae5010e6?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTd8fHRlbCUyMGF2aXZ8ZW58MHx8MHx8fDA%3D',
 	'https://images.unsplash.com/photo-1632700081118-023072acc02a?q=80&w=2340&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
 	'https://images.unsplash.com/photo-1652550118215-16a41e067804?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
