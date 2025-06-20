cc974f7f776f26238cb2580ae3f09e729009072c
לוי מאיר קלנסי <32032952+levimeirclancy@users.noreply.github.com>
2025-06-19 17:11:55 -0700

    Update index.php

--- index.php
+++ index.php
@@ -2,21 +2,12 @@
 
 $index_temp = "valid";
 
-if !($_SESSION['csv_contents'])):
-	$csv_contents = file_get_contents('https://docs.google.com/spreadsheets/d/e/2PACX-1vT2qh0dMlZ6gI8aEqqMjQt6ZS6U-sYOX3AKOHz1ajVgiJ0PfiNAQpp5B1-a8yAZ-e81DPMCs-b-aS8H/pub?output=csv');
-	$_SESSION['csv_contents'] = $csv_contents;
-	endif;
+// if !($_SESSION['csv_contents'])):
+//	$csv_contents = file_get_contents('https://docs.google.com/spreadsheets/d/e/2PACX-1vT2qh0dMlZ6gI8aEqqMjQt6ZS6U-sYOX3AKOHz1ajVgiJ0PfiNAQpp5B1-a8yAZ-e81DPMCs-b-aS8H/pub?output=csv');
+//	$_SESSION['csv_contents'] = $csv_contents;
+//	endif;
 
-print_r($csv_contents);
-
-// Home
-// About
-// Shop
-
-// Telegram channel
-// WhatsApp channel
-// Subreddit
-// Bluesky
+// print_r($csv_contents);
 
 if (empty($url_chooser)):
 	include_once('index-home.php');
