8f71ec7a9e774f0b7d58eab1afe14f996db1d4b7
לוי מאיר קלנסי <32032952+levimeirclancy@users.noreply.github.com>
2025-06-19 19:18:21 -0700

    Update style-photo.php

--- style-photo.php
+++ style-photo.php
@@ -1,3 +1,16 @@
 <?
 
+$css_array = [
+
+".hashtag-white" => [
+	"background"			=> "linear-gradient(45deg,rgba(255, 255, 255, 1), rgba(215, 215, 215, 1))",
+	],
+
+];
+ 
+$html_output .= css_process($css_array);
+
+?>
+
+
 ?>
